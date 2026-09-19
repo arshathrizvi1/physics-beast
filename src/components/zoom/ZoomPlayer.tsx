0703e14554b63ce3855a050c007efa032db0d31d
arshathrizvi1 <arshathrizvi1010@gmail.com>
2026-09-19 20:08:34 +0530

    fix: entirely remove custom V3 layout engine and automate clicking native Zoom 'View Options -> Fit to Window / Standard' to perfectly resolve screenshare bounds natively

--- src/components/zoom/ZoomPlayer.tsx
+++ src/components/zoom/ZoomPlayer.tsx
@@ -38,7 +38,7 @@ export default function ZoomPlayer({
     const isMobileDevice = mobileCheck || capCheck;
     setIsMobile(isMobileDevice);
 
-    if (capCheck || mobileCheck) { window.location.href = "/zoom-mobile.html?v=65&" + params.toString(); } else { setPermissionsGranted(true); }
+    if (capCheck || mobileCheck) { window.location.href = "/zoom-mobile.html?v=66&" + params.toString(); } else { setPermissionsGranted(true); }
   }, []);
 
   useEffect(() => {
